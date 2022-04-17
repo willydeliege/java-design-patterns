7c4fa963bd7e5e06569ef264086e3f3e0b96b3dd
willydeliege <frederic.willem@gmail.com>
2022-04-17 10:03:37 +0200

    add license

--- sharding/src/main/java/com/iluwatar/sharding/ShardManager.java
+++ sharding/src/main/java/com/iluwatar/sharding/ShardManager.java
@@ -1,27 +1,4 @@
 /*
-*The MIT License
-*Copyright © 2014-2021 Ilkka Seppälä
-*
-*Permission is hereby granted, free of charge, to any person obtaining a copy
-*of this software and associated documentation files (the "Software"), to deal
-*in the Software without restriction, including without limitation the rights
-*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*copies of the Software, and to permit persons to whom the Software is
-*furnished to do so, subject to the following conditions:
-*
-*The above copyright notice and this permission notice shall be included in
-*all copies or substantial portions of the Software.
-*
-*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*THE SOFTWARE.
-*/
-
-/*
  * The MIT License
  * Copyright © 2014-2021 Ilkka Seppälä
  *
@@ -46,13 +23,12 @@
 
 package com.iluwatar.sharding;
 
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Abstract class for ShardManager.
- */
+import java.util.HashMap;
+import java.util.Map;
+
+/** Abstract class for ShardManager. */
 @Slf4j
 public abstract class ShardManager {
 
@@ -66,16 +42,11 @@ public abstract class ShardManager {
    * Add a provided shard instance to shardMap.
    *
    * @param shard new shard instance.
-   * @return {@code true} if succeed to add the new instance.
-   *         {@code false} if the shardId is already existed.
    */
-  public boolean addNewShard(final Shard shard) {
+  public void addNewShard(final Shard shard) {
     var shardId = shard.getId();
     if (!shardMap.containsKey(shardId)) {
       shardMap.put(shardId, shard);
-      return true;
-    } else {
-      return false;
     }
   }
 
@@ -119,5 +90,4 @@ public abstract class ShardManager {
    * @return id of shard that the data should be stored
    */
   protected abstract int allocateShard(final Data data);
-
 }
